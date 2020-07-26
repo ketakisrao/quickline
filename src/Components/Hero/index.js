dedbd11745236223eb2f295747563d80c205b7cc
ketakisrao <ketaki_8@hotmail.com>
2020-07-26 04:45:07 -0400

    Styles for Hero component updated

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -9,27 +9,29 @@ function Hero(props) {
     var info = props.info;
     return (
         <div className="hero">
-                <div>
-                    <Container>
-                        <Row>
-                            <Col xs={12} sm={12} md={8} lg={8} xl={8}>
-                                <div className="title">
-                                    <div>
-                                        <h3>Prototype</h3>
-                                        <p>We ideated on the features of each prototype that
-                                        would be most useful to our duel channel solution.
-                                        We conducted a competitive analysis of existing best-in-class
-                                        airport applications and secondary research into existing virtual
+            <div>
+                <Container>
+                    <Row>
+                        <Col xs={12} sm={12} md={8} lg={8} xl={8}>
+                            <div className="title">
+                                <div>
+                                    <h3>Prototype</h3>
+                                    <p>We ideated on the features of each prototype that
+                                    would be most useful to our duel channel solution.
+                                    We conducted a competitive analysis of existing best-in-class
+                                    airport applications and secondary research into existing virtual
                                         queue system technology to inform our design decisions.</p>
-                                    </div>
                                 </div>
-                            </Col>
-                            <Col xs={12} sm={12} md={4} lg={4} xl={4}>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={12} md={4} lg={4} xl={4}>
+                            <div class="centered-item">
                                 <img src={HeroImg} className="full-width" />
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
